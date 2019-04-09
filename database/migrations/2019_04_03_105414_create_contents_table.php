d7347a66c3addee38b22c69394906bab30192d11
ajit7 <ajitdas2900@gmail.com>
2019-04-09 17:26:14 +0600

    initial commit

---
+++ database/migrations/2019_04_03_105414_create_contents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('url');
+            $table->text('description');
+            $table->string('image');
+            $table->integer('user_id');
+            $table->integer('sub_category_id')->nullable();
+            $table->integer('city_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
